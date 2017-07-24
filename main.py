16187500db1e79ec5b34376d9055977f14f3123f
Aaron Madison <aaron.l.madison@gmail.com>
2017-07-24 10:19:23 -0500

    changed to teal

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<body style=\'background-color: magenta\'>Hello, World!</body>')
+        self.response.write('<body style=\'background-color: teal\'>Hello, World!</body>')
 
 
 app = webapp2.WSGIApplication([
